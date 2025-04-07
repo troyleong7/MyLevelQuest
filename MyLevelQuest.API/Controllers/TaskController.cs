77943ca80d4a825a5050f4f23f1d841d60fb9044
troyleong7 <yunkengl@student.unimelb.edu.au>
2025-04-08 00:32:48 +1000

    add authorization to task

--- MyLevelQuest.API/Controllers/TaskController.cs
+++ MyLevelQuest.API/Controllers/TaskController.cs
@@ -1,10 +1,12 @@
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyLevelQuest.API.Models;
 using MyLevelQuest.API.Repositories;
 
 namespace MyLevelQuest.API.Controllers
-{
+{   
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class TaskController : ControllerBase
